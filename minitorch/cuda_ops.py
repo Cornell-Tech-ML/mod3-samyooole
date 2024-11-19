91e4e505ad47c2d40c45d1362ea77d5baff03000
Samuel Ho <samuelho@Samuels-MacBook-Pro.local>
2024-11-19 17:06:50 -0500

    lala

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -531,46 +531,48 @@ def _tensor_matrix_multiply(
 
     assert a_shape[-1] == b_shape[-2]
 
-    # Initialize the output value
-    out_value = 0.0
 
-    # Iterate over all tiles
-    for block_i in range((a_shape[-1] + BLOCK_DIM - 1) // BLOCK_DIM):
-        # Load a tile of matrix A into shared memory
-        if i < a_shape[-2] and (block_i * BLOCK_DIM + pj) < a_shape[-1]:
-            a_shared[pi, pj] = a_storage[
+    acc = 0.0
+
+
+    for k_start in range(0, a_shape[-1], BLOCK_DIM):
+
+        a_shared[pi, pj] = 0
+        b_shared[pi, pj] = 0
+        cuda.syncthreads()
+
+
+        if i < a_shape[-2] and (k_start + pj) < a_shape[-1]:
+            a_pos = (
                 batch * a_batch_stride
-                + i * a_strides[1]
-                + (block_i * BLOCK_DIM + pj) * a_strides[2]
-            ]
-        else:
-            a_shared[pi, pj] = 0.0
+                + i * a_strides[-2]
+                + (k_start + pj) * a_strides[-1]
+            )
+            a_shared[pi, pj] = a_storage[a_pos]
 
-        # Load a tile of matrix B into shared memory
-        if j < b_shape[-1] and (block_i * BLOCK_DIM + pi) < b_shape[-2]:
-            b_shared[pi, pj] = b_storage[
+
+        if (k_start + pi) < b_shape[-2] and j < b_shape[-1]:
+            b_pos = (
                 batch * b_batch_stride
-                + (block_i * BLOCK_DIM + pi) * b_strides[1]
-                + j * b_strides[2]
-            ]
-        else:
-            b_shared[pi, pj] = 0.0
+                + (k_start + pi) * b_strides[-2]
+                + j * b_strides[-1]
+            )
+            b_shared[pi, pj] = b_storage[b_pos]
 
-        # Synchronize to ensure all threads have loaded their data into shared memory
         cuda.syncthreads()
 
-        # Compute partial product for the tile
-        for k in range(BLOCK_DIM):
-            out_value += a_shared[pi, k] * b_shared[k, pj]
 
-        # Synchronize again before loading the next tile
+        if i < a_shape[-2] and j < b_shape[-1]:
+            for k in range(min(BLOCK_DIM, a_shape[-1] - k_start)):
+                acc += a_shared[pi, k] * b_shared[k, pj]
+
         cuda.syncthreads()
 
-    # Write the result to global memory
-    if i < out_shape[-2] and j < out_shape[-1]:
-        out[batch * out_strides[0] + i * out_strides[1] + j * out_strides[2]] = (
-            out_value
-        )
+
+    if i < a_shape[-2] and j < b_shape[-1]:
+        out_pos = batch * out_strides[0] + i * out_strides[-2] + j * out_strides[-1]
+        out[out_pos] = acc
+
 
 
 tensor_matrix_multiply = cuda.jit(_tensor_matrix_multiply)
