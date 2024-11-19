b0a9ff80778c915882097f4557f42c51b9bcb699
Samuel Ho <samuelho@dhcp-vl2051-445.eduroam.cornell.edu>
2024-11-19 10:15:46 -0500

    gpu overflow issues

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -365,7 +365,7 @@ def tensor_reduce(
         reduce_dim: int,
         reduce_value: float,
     ) -> None:
-        BLOCK_DIM = 1024
+        BLOCK_DIM = 256
         cache = cuda.shared.array(BLOCK_DIM, numba.float32)
         out_index = cuda.local.array(MAX_DIMS, numba.int32)
         out_pos = cuda.blockIdx.x
